c14951c978ca912abb36727bdcaa847984e08942
marcos.gonzalez <marcos.gonzalez@alumnos.uneatlantico.es>
2026-09-16 08:53:23 +0200

    primeros cambios

---
+++ EmpezarVisualStudio/Clase1 --- 16-09/PrimeraSolucion/Class1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoConsola1
+{
+
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2");
+        }
+    }
+
+}
---
+++ EmpezarVisualStudio/Clase1 --- 16-09/PrimeraSolucion/PrimeraSolucion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoConsola1
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello, World!");
+            string imput = Console.ReadLine();
+            Console.WriteLine(imput);
+
+        }
+    }
+}
